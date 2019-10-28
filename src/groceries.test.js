104576be5688d26efde4d5a4c4003d573988d94a
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-10-27 20:18:26 -0400

    Add reducer test for item removal

--- src/groceries.test.js
+++ src/groceries.test.js
@@ -1,5 +1,5 @@
 import rootReducer from './reducers';
-import { initialState, ADD_ITEM } from './ducks/groceries';
+import { initialState, ADD_ITEM, REMOVE_ITEM } from './ducks/groceries';
 
 describe('groceries reducer', () => {
   it('should return the initial state', () => {
@@ -43,6 +43,46 @@ describe('groceries reducer', () => {
       rootReducer(startingState, {
         type: ADD_ITEM,
         payload: item
+      })
+    ).toEqual(expectedState);
+  });
+
+  it('should remove grocery list item', () => {
+    const startingState = {
+      groceries: {
+        list: [
+          {
+            id: 66,
+            name: 'Bananas',
+            category: 'Fruit',
+            deliveryMethod: 'Air',
+          },
+          {
+            id: 16,
+            name: 'Whole Grain Bread',
+            category: 'Grains',
+            deliveryMethod: 'Air',
+          }
+        ]
+      }
+    };
+    const expectedState = {
+      groceries: {
+        list: [
+          {
+            id: 16,
+            name: 'Whole Grain Bread',
+            category: 'Grains',
+            deliveryMethod: 'Air',
+          }
+        ]
+      }
+    };
+
+    expect(
+      rootReducer(startingState, {
+        type: REMOVE_ITEM,
+        payload: 66
       })
     ).toEqual(expectedState);
   });
