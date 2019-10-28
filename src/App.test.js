ac68100fd27494bacc4894d0acc8ff74308d4039
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-10-27 22:31:49 -0400

    Add desribe block to App component test

--- src/App.test.js
+++ src/App.test.js
@@ -6,8 +6,10 @@ import configureStore from "./store/configure-store";
 
 const store = configureStore();
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Provider store={store}><App /></Provider>, div);
-  ReactDOM.unmountComponentAtNode(div);
+describe('<App />', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Provider store={store}><App /></Provider>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
 });
