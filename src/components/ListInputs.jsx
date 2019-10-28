253c064728c0ac32761df3833dfa680f7ecd8093
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-10-27 22:01:01 -0400

    Create random ids to help prevent id clashes with new items
    Should be switched to something more random and unique like uuids, but this should suffice for demonstration purposes

--- src/components/ListInputs.jsx
+++ src/components/ListInputs.jsx
@@ -3,8 +3,13 @@ import PropTypes from 'prop-types';
 
 export const ListInputs = ({ addItem }) => {
   const createItem = () => {
+    // KLUDGE: Create new "unique" ids to prevent id clashes. We should
+    // ideally be using more certain random id creation with uuids or
+    // something similar.
+    const id = Math.floor(Math.random() * Math.floor(1000));
+
     const item = {
-      id: 10,
+      id,
       name: 'Roasted Turkey',
       category: 'Deli',
       deliveryMethod: 'Ground',
