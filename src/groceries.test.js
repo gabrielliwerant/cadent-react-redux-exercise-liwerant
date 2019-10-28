10b4c2d533793b95b85bd19d15099370cf8497d0
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-10-27 20:01:11 -0400

    Add test for adding grocery item

--- src/groceries.test.js
+++ src/groceries.test.js
@@ -1,8 +1,49 @@
 import rootReducer from './reducers';
-import { initialState } from './ducks/groceries';
+import { initialState, ADD_ITEM } from './ducks/groceries';
 
 describe('groceries reducer', () => {
   it('should return the initial state', () => {
     expect(rootReducer(undefined, {})).toEqual({groceries: initialState});
+  });
+
+  it('should add grocery list item', () => {
+    const startingState = {
+      groceries: {
+        list: [
+          {
+            id: 66,
+            name: 'Bananas',
+            category: 'Fruit',
+            deliveryMethod: 'Air',
+          }
+        ]
+      }
+    };
+    const item = {
+      id: 10,
+      name: 'Roasted Turkey',
+      category: 'Deli',
+      deliveryMethod: 'Ground',
+    };
+    const expectedState = {
+      groceries: {
+        list: [
+          {
+            id: 66,
+            name: 'Bananas',
+            category: 'Fruit',
+            deliveryMethod: 'Air',
+          },
+          item
+        ]
+      }
+    };
+
+    expect(
+      rootReducer(startingState, {
+        type: ADD_ITEM,
+        payload: item
+      })
+    ).toEqual(expectedState);
   });
 });
