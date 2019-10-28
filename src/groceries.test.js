db812d7f86a49cc824c231eb62566f02786f74f6
Gabriel Liwerant <gabriel@abrightconcept.com>
2019-10-27 20:42:48 -0400

    Add tests for grocery list item selection

--- src/groceries.test.js
+++ src/groceries.test.js
@@ -1,5 +1,5 @@
 import rootReducer from './reducers';
-import { initialState, ADD_ITEM, REMOVE_ITEM } from './ducks/groceries';
+import { initialState, ADD_ITEM, REMOVE_ITEM, SELECT_ITEM } from './ducks/groceries';
 
 describe('groceries reducer', () => {
   it('should return the initial state', () => {
@@ -83,6 +83,126 @@ describe('groceries reducer', () => {
       rootReducer(startingState, {
         type: REMOVE_ITEM,
         payload: 66
+      })
+    ).toEqual(expectedState);
+  });
+
+  it('should select grocery list item when none have been selected yet', () => {
+    const startingState = {
+      groceries: {
+        list: [
+          {
+            id: 66,
+            name: 'Bananas',
+            category: 'Fruit',
+            deliveryMethod: 'Air',
+          },
+          {
+            id: 16,
+            name: 'Whole Grain Bread',
+            category: 'Grains',
+            deliveryMethod: 'Air',
+          }
+        ],
+        isItemSelected: false,
+        selectedItem: {
+          id: 0,
+          name: '',
+          category: '',
+          deliveryMethod: '',
+        },
+      }
+    };
+    const expectedState = {
+      groceries: {
+        list: [
+          {
+            id: 66,
+            name: 'Bananas',
+            category: 'Fruit',
+            deliveryMethod: 'Air',
+          },
+          {
+            id: 16,
+            name: 'Whole Grain Bread',
+            category: 'Grains',
+            deliveryMethod: 'Air',
+          }
+        ],
+        isItemSelected: true,
+        selectedItem: {
+          id: 66,
+          name: 'Bananas',
+          category: 'Fruit',
+          deliveryMethod: 'Air',
+        },
+      }
+    };
+
+    expect(
+      rootReducer(startingState, {
+        type: SELECT_ITEM,
+        payload: 66
+      })
+    ).toEqual(expectedState);
+  });
+
+  it('should select new grocery list item when selecting a different one from that which is already selected', () => {
+    const startingState = {
+      groceries: {
+        list: [
+          {
+            id: 66,
+            name: 'Bananas',
+            category: 'Fruit',
+            deliveryMethod: 'Air',
+          },
+          {
+            id: 16,
+            name: 'Whole Grain Bread',
+            category: 'Grains',
+            deliveryMethod: 'Air',
+          }
+        ],
+        isItemSelected: true,
+        selectedItem: {
+          id: 66,
+          name: 'Bananas',
+          category: 'Fruit',
+          deliveryMethod: 'Air',
+        },
+      }
+    };
+    const expectedState = {
+      groceries: {
+        list: [
+          {
+            id: 66,
+            name: 'Bananas',
+            category: 'Fruit',
+            deliveryMethod: 'Air',
+          },
+          {
+            id: 16,
+            name: 'Whole Grain Bread',
+            category: 'Grains',
+            deliveryMethod: 'Air',
+          }
+        ],
+        isItemSelected: true,
+        selectedItem: {
+          id: 16,
+          name: 'Whole Grain Bread',
+          category: 'Grains',
+          deliveryMethod: 'Air',
+        },
+      }
+    };
+
+    expect(
+      rootReducer(startingState, {
+        type: SELECT_ITEM,
+        payload: 16
       })
     ).toEqual(expectedState);
   });
